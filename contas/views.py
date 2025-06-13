22156ce567115276ed6c41b4191a2a15554ff154
cavalcantetavares <auvarotavares@gmail.com>
2025-06-13 13:30:11 -0300

    mudança views.py

--- contas/views.py
+++ contas/views.py
@@ -1,3 +1,4 @@
-from django.shortcuts import render
-from django
+from django.shortcuts import render , redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages # Para exibir mensagens ao usuario
 # Create your views here.
