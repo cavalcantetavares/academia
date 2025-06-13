95a956282bc43ca8b1af8754afbf5784f00b471c
cavalcantetavares <auvarotavares@gmail.com>
2025-06-13 13:36:38 -0300

    mudança views.py

--- contas/views.py
+++ contas/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render , redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages # Para exibir mensagens ao usuario
-# Create your views here.
+
+
+def resgistrar ( request):
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid();
+        form.save() # Salva o novo usuario no banco de dados
+        username = form.cleaned_data.get('username')
