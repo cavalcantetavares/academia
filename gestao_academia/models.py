42b0f29f9501ed403c7bfba7b58faad194bb413e
cavalcantetavares <auvarotavares@gmail.com>
2025-06-24 13:51:00 -0300

    terminando models.py

--- gestao_academia/models.py
+++ gestao_academia/models.py
@@ -34,3 +34,29 @@ class Faixa(models.Model):
    def __str__(self):
      return f"{self.modalidade.name} - {self.name}"
 # NOVO MODELO: ALUNO
+class Aluno(models.Model):
+    nome_completo = models.CharField(max_length=255, verbose_name="Nome Completo")
+    data_nascimento = models.DateField(verbose_name="Data de Nascimento")
+    telefone = models.CharField(max_length=20, blank=True, null=True, verbose_name="Telefone")
+    email = models.EmailField(max_length=255, unique=True, verbose_name="E-mail")
+    data_matricula = models.DateField(defeult=timezone.now, verbose_name="Data da Matrícula")
+    plano = models.ForeignKey(Plano, on_delete=models.SET_NULL, null=True, blank=True, related_name="alunos",verbose_name="Plano Atual")
+
+    def __str__(self):
+        return self.name_completo
+
+# NOVO MODELO: MatrículaModalidade (Tabela Intermediária)
+# Esta tabela especial liga um Aluno a uma Modalidade e guarda a Faixa do aluno NESSA modalidade específica.
+ 
+class MatriculaModalidade(models.Model):
+   aluno = models.ForeignKey(Aluno, on_delete=models.CASCADE)
+   modalidade = models.ForeignKey(Modalidade, on_delete=models.CASCADE)
+   faixa = models.ForeignKey(Faixa, on_delete=models.SET_NULL, null=True, blank=True)
+   data_inicio = models.DateField(default=timezone.now)
+
+
+   class Meta:
+      unique_together = ('aluno', 'modalidade')  # Um aluno só pode se matricular uma vez em cada modalidade
+
+   def __str__(self):
+      return f"{self.aluno.nome_completo} em {self.modalidade.nome}"      
