dc492139135a4690e4bd0811862f1fbdf7f9651e
cavalcantetavares <auvarotavares@gmail.com>
2025-06-24 13:31:16 -0300

    novas funcionalidades

--- gestao_academia/models.py
+++ gestao_academia/models.py
@@ -1,8 +1,36 @@
+# gestao_academia/models.py
 from django.db import models
+from django.utils import timezone #Para usar a data atual
+
 
 class Modalidade(models.Model):
    nome  = models.CharField(max_length=100,unique=True, help_text="Nome da modalidade (Ex: Judô, Jiu Jitsu.)")
    descricao = models.TextField(blank=True,null=True, help_text="Uma breve descrição da modalidade.")
+   utiliza_faixas = models.BooleanField(default=False, verdose_name="Utiliza Faixas") 
 
    def __str__(self):
        return self.nome
+   
+# NOV MODELO : PLANO
+class Plano(models.Model):utiliza
+   nome = models.CharField(max_length=100, unique=True, verbose_name="Nome do Plano")
+   valor = models.DecimalField(max_digits=8, decimal_places=2, verbose_name="Valor Mensal")
+   duracao_meses = models.IntegerField(default=1, verbose_name="Duração (meses)") 
+    
+    def __str__(self):
+       return f"{self.nome} - R${self.valor}/mês"
+   
+# NOVO MODELO: FAIXA
+class Faixa(models.Model):
+   modalidade = models.ForeignKey(Modalidade, on_delete=models.CASCADE, related_name="faixas")
+   name = models.CharField(max_length=50, verbose_name="Nome da Faixa")
+   ordem = models.PositiveIntegerField(help_text="Usado para ordenação (ex: Branca=1, Azul=2, etc.)")
+     
+   class Meta:
+    # Garante que não há faixas com o mesmo nome para a mesma modalidade
+       unique_together = ('modalidade', 'name')
+       ordering = ['ordem']
+
+   def __str__(self):
+     return f"{self.modalidade.name} - {self.name}"
+# NOVO MODELO: ALUNO
